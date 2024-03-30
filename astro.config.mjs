d645e7cb339395dc418af025bf17345b69e0f71e
Ing. Rosdanys Ramirez Arcas <rosdanys@gmail.com>
2024-03-30 15:13:36 -0400

    fix : error by node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import react from "@astrojs/react";
 
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), compress(), react()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
 });
